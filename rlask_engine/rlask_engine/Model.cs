0a7868dc0dc4cd584757a66db4d18c01f88105ce
Fedor Nikitin <2205807@edu.karelia.fi>
2022-12-30 22:11:27 +0400

    initial commit

---
+++ rlask_engine/rlask_engine/Model.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rlask_engine
+{
+
+    public class InvoicingContext : DbContext
+    {
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Contractor> Contractors { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<InvoiceRow> InvoiceRows { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public InvoicingContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(Environment.GetEnvironmentVariable("connectionString"));
+    }
+
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public Contractor Contractor { get; set; }
+        public Customer Customer { get; set; }
+        public List<InvoiceRow> InvoiceRows { get; } = new();
+        public string ExtraDetails { get; set; }
+        public decimal TotalSum { get => InvoiceRows.Count() == 0 ? 0 : InvoiceRows.Sum(n => n.Total); }
+
+        public override string ToString()
+        {
+            return $"LASKU\n" +
+                $"Laskuttaja\n{Contractor.ContractorName}\n" +
+                $"{Contractor.ContractorAddress}\n" +
+                $"Päiväys: {InvoiceDate:dd.MM.yyyy}\n" +
+                $"Laskun numero: {InvoiceId}\n" +
+                $"Eräpäivä: {DueDate:dd.MM.yyyy}\n\n" +
+                $"Asiakas:\n{Customer.CustomerName}\n" +
+                $"{Customer.CustomerAddress}\n\n" +
+                $"Lisätiedot: {ExtraDetails}\n\n" +
+                $"{RowsGridView()}\n" +
+                $"YHTEENSÄ: {TotalSum}\n\n";
+        }
+
+        
+
+        public string RowsGridView()
+        {
+            string Pad(string[] values, int Width) => $"|{values[0].PadRight(Width, ' ')}|{values[1],20}|{values[2],15}|{values[3],15}|{values[4],15}|";
+            
+            int width = InvoiceRows.Select(n => n.Product.ProductName).Select(n => n.Length).Max();
+            string headers = Pad(new string[] { "Tuote", "Määrä", "Yksikkö", "A-hinta", "Yhteensä" }, width);
+            string[] rows = InvoiceRows.Select(n => Pad(n.GetValuesAsArray(), width)).ToArray();
+
+            return headers + '\n' + string.Join('\n', rows);
+        }
+    }
+
+    public class Contractor
+    {
+        public int ContractorId { get; set; }
+        public string ContractorName { get; set; }
+        public string ContractorAddress { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+
+    public class InvoiceRow
+    {
+        public int InvoiceRowId { get; set; }
+        public Product Product { get; set; }
+        public decimal Amount { get; set; }
+        public Invoice Invoice { get; set; }
+        public decimal Total { get; set; }
+        public string[] GetValuesAsArray() => 
+            new string[] { Product.ProductName, Amount.ToString("N2"), Product.Unit, Product.UnitPrice.ToString("N2"), Total.ToString("N2") };
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Unit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public bool IsMaterial { get; set; }
+        public override string ToString() => $"Tuote: {ProductName} kappalehinta: {UnitPrice:N2}/{Unit}";
+    }
+}
---
+++ rlask_engine/rlask_engine/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace rlask_engine
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string help = "Komentot: \n" +
+                "l - Kaikkien laskutietojen hakeminen ja listaaminen\n" +
+                "o - Yksittäisen laskun laskutietojen hakeminen ja listaaminen\n" +
+                "a - Yksittäisen asiakkaan kaikkien laskujen hakeminen ja listaaminen\n" +
+                "t - Kaikkien järjestelmään tallennettujen tuotetietojen listaaminen\n" +
+                "i - Laskutietojen hakeminen tuotteen perusteella\n" +
+                "s - Testidatan tallentaminen tietokantaan";
+
+            Console.WriteLine(help);
+           
+            do
+            {
+                Console.Write("> ");
+                var command = Console.ReadLine();
+                switch (command)
+                {
+                    
+                    case "l":
+                        Console.WriteLine(Commands.ListAllInvoices());
+                        break;
+                    case "o":
+                        Console.WriteLine(Commands.GetInvoiceById());
+                        break;
+                    case "a":
+                        Console.WriteLine(Commands.ListInvoicesByCustomerId());
+                        break;
+                    case "t":
+                        Console.WriteLine(Commands.ListAllProducts());
+                        break;
+                    case "i":
+                        Console.WriteLine(Commands.ListInvoicesByProductId());
+                        break;
+                    case "s":
+                        Commands.SeedTestData();
+                        break;
+                    default:
+                        Console.WriteLine(help);
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
